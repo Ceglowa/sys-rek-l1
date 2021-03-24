d895060e21bbf20e88cb5f24c2f7ec50c653961f
Filip Dratwiński <dratwinski.filip@gmail.com>
2021-03-24 15:19:03 +0100

    Implement Deep Factoization Machine

--- data.py
+++ data.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import surprise
+import torch
 from surprise import Dataset
 from typing import Tuple
+import numpy as np
+import torch.utils.data
 
 
 def get_movielens_1m() -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
@@ -29,3 +32,23 @@ def get_movielens_1m() -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     )
 
     return movies_df, users_df, ratings_df
+
+
+class MovieLens1MDataset(torch.utils.data.Dataset):
+
+    def __init__(self):
+        _, _, ratings_df = get_movielens_1m()
+
+        self.items = ratings_df.iloc[:, :2].astype(np.longlong) - 1  # -1 because ID begins from 1
+        self.ratings = ratings_df.iloc[:, 2]
+
+        self.dims = np.max(self.items, axis=0) + 1
+
+        self.user_field_idx = np.array((0,), dtype=np.longlong)
+        self.item_field_idx = np.array((1,), dtype=np.longlong)
+
+    def __len__(self):
+        return self.ratings.shape[0]
+
+    def __getitem__(self, index):
+        return np.array(self.items.iloc[index]), np.array(self.ratings.iloc[index])
